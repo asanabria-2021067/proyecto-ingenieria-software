3cd39af27b97a88024ff850d2d728f2a9760ccea
Angel Sanabria <as1945228@gmail.com>
2026-04-17 21:44:12 -0600

    Revert "Merge pull request #62 from asanabria-2021067/feature/perfil"
    This reverts commit 82d075c5c0e454e63c87e8cf65de517aea67771d, reversing
    changes made to df13d340bce6b9ed63dd38f3e2c5e1b932ef6788.

--- apps/backend/src/users/users.controller.ts
+++ apps/backend/src/users/users.controller.ts
@@ -28,16 +28,14 @@ export class UsersController {
     return this.usersService.getMe(user.userId);
   }
 
-  @Get('me/perfil/bootstrap')
-  getProfileBootstrap(@CurrentUser() user: { userId: number }) {
-    return this.usersService.getProfileBootstrap(user.userId);
-  }
-
   @Patch('me/perfil')
-  async updateProfile(
+  updateProfile(
     @CurrentUser() user: { userId: number },
     @Body() dto: UpdateProfileDto,
   ) {
+    if (dto.fotoUrl) {
+      this.usersService.updateFotoUrl(user.userId, dto.fotoUrl);
+    }
     return this.usersService.updateProfile(user.userId, dto);
   }
 
