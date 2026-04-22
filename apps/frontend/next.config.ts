783e411183d9fb18196b62647786f64548f4d46c
Angel Sanabria <as1945228@gmail.com>
2026-04-21 20:34:32 -0600

    feat: add additional remote image patterns for Cloudinary and GitHub in next.config.ts

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -9,6 +9,21 @@ const nextConfig: NextConfig = {
         hostname: 'res.cloudinary.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'user-images.githubusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        pathname: '/**',
+      },
     ],
   },
 };
